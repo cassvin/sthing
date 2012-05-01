d3d02c75112b756efeb05e936ba5e62624daf065
Leon Hui <cassvin91@gmail.com>
2012-05-02 01:29:13 +0800

    color.py -- a simple script which can print color(0 ~ 255)

---
+++ color.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+print 'System color:'
+SCHEMA = '\x1b[48;5;%dm '
+
+for color in range(8):
+	print SCHEMA % color,
+print '\x1b[0m'
+for color in range(8, 16):
+	print SCHEMA % color,
+print '\x1b[0m\n'
+
+
+print 'Color cube(6x6x6):'
+for green in range(6):
+	for red in range(6):
+		for blue in range(6):
+			color = 16 + red * 36 + green * 6 + blue
+			print SCHEMA % color,
+		print '\x1b[0m',
+	print '\n',
+
+print '\nGrayscale:'
+for color in range(232, 256):
+	print SCHEMA % color,
+print '\x1b[0m\n'
+
